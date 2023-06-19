95884219d5fdbfcbb5482be9f6d5b23716ab3df9
Alexana7 <ngayane@bk.ru>
2023-06-19 15:16:46 +0300

    add feat: show/hide preloader and button show-more

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// настройки
+
 const url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
 const apiKey = '19c43354-563e-4c55-b9ff-4e50b69a8dbc';
 const options = {
@@ -8,8 +10,43 @@ const options = {
     },
 }
 
-const moviesWrapper = document.querySelector('.films');
+// DOM elements
 
+const moviesWrapper = document.querySelector('.films');
+const loader = document.querySelector('.loader-wrapper');
+const btnShowMore = document.querySelector('.show-more');
+btnShowMore.onclick = fetchAndRenderMovies;
+
+let pageNumber = 1;
+
+
+// get and render TOP 250 movies
+async function fetchAndRenderMovies() {
+
+    // show preloader
+    loader.classList.remove('none')
+
+    //fetch movies data
+    const data = await fetchData(`${url}top?page=${pageNumber}`, options);
+    console.log(pageNumber)
+    if (data.pagesCount > 1) pageNumber++
+    
+    console.log(data)
+
+    ////render button 'show-more', if the next page is availible
+     if(data.pagesCount > 1) btnShowMore.classList.remove('none')
+
+    //Hide preloader
+    
+    loader.classList.add('none')
+
+    //render movies on page
+    renderMovies(data.films)
+
+    //hide button 'show-more', if the next page is not availible
+    if(pageNumber > data.pagesCount) btnShowMore.classList.add('none')
+
+}
 async function fetchData(url, options) {
     const response = await fetch(url, options);
     const data = await response.json();
@@ -17,18 +54,11 @@ async function fetchData(url, options) {
 
 }
 
-async function fetchAndRenderMovies() {
-    const data = await fetchData(`${url}top`, options)
-    renderMovies(data.films)
-
-}
-
 function renderMovies(movies) {
     for (movie of movies) {
-        console.log(movie)
         const html = `<div class="card">
                         <img class="card-image" src=${movie.posterUrlPreview}>
-                        <h3 class="card-title">${movie.nameEn}</h3>
+                        <h3 class="card-title">${movie.nameRu}</h3>
                         <p class="card-year">${movie.year}</p>
                         <p class="card-rate">Рейтинг: ${movie.rating}</p>
                     </div>`;
@@ -37,7 +67,5 @@ function renderMovies(movies) {
 
 }
    
-
-
 
 fetchAndRenderMovies().catch(err => console.log(err));
