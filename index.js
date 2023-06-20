4bbc57a51df6fee1c017eae85409697509af0b8a
Alexana7 <ngayane@bk.ru>
2023-06-20 15:35:12 +0300

    add movie-details and formatted movie duration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // настройки
 
 const url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
-const apiKey = '19c43354-563e-4c55-b9ff-4e50b69a8dbc';
+const apiKey = 'f4f926d1-0460-4a27-99a0-e32174cd75fa';
 const options = {
     method: 'GET',
     headers: {
@@ -17,6 +17,7 @@ const loader = document.querySelector('.loader-wrapper');
 const btnShowMore = document.querySelector('.show-more');
 btnShowMore.onclick = fetchAndRenderMovies;
 
+
 let pageNumber = 1;
 
 
@@ -28,10 +29,7 @@ async function fetchAndRenderMovies() {
 
     //fetch movies data
     const data = await fetchData(`${url}top?page=${pageNumber}`, options);
-    console.log(pageNumber)
     if (data.pagesCount > 1) pageNumber++
-    
-    console.log(data)
 
     ////render button 'show-more', if the next page is availible
      if(data.pagesCount > 1) btnShowMore.classList.remove('none')
@@ -55,17 +53,99 @@ async function fetchData(url, options) {
 }
 
 function renderMovies(movies) {
+    let movie;
     for (movie of movies) {
-        const html = `<div class="card">
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+        
+        card.id = movie.filmId;
+        card.onclick = openMovieDetails;
+
+        const html = `
                         <img class="card-image" src=${movie.posterUrlPreview}>
                         <h3 class="card-title">${movie.nameRu}</h3>
                         <p class="card-year">${movie.year}</p>
                         <p class="card-rate">Рейтинг: ${movie.rating}</p>
-                    </div>`;
-        moviesWrapper.insertAdjacentHTML('beforeend', html)
-        }
+                    `;
+
+        card.insertAdjacentHTML('afterbegin', html)
+        moviesWrapper.insertAdjacentElement('beforeend', card)
+    }
 
 }
-   
 
+async function openMovieDetails(e) {
+
+    //get movie id
+   const id = e.currentTarget.id;
+
+   //get movie data by id
+    const movieDetails =  await fetchData(url + id, options); 
+    console.log(movieDetails)
+
+
+    renderMovieData(movieDetails);
+   
+}
+
+function renderMovieData (movie) {
+
+    // delete container, if it's already wes rendered
+    document.querySelector('.container-right') && document.querySelector('.container-right').remove();
+
+    //1. render container-right
+    
+    const containerRight = document.createElement('div');
+
+    //2. render button close, delete container by click
+    
+    containerRight.classList.add('container-right');
+    document.body.insertAdjacentElement('beforeend', containerRight);
+    
+    const btnClose = document.createElement('button');
+    btnClose.classList.add('button-close');
+    btnClose.innerHTML = '<img src="./img/cross.svg" alt="close" width="24">';
+    containerRight.insertAdjacentElement('afterbegin', btnClose);
+
+    btnClose.onclick = () => {
+        containerRight.remove()
+    }
+
+    //3. film-details 
+
+    const html = `
+                    <div class="film">
+                        <div class="film__title">${movie.nameRu}</div>
+                        <div class="film__img">
+                            <img src=${movie.posterUrl} alt="Cover">
+                        </div>
+                        <div class="film__desc">
+                            <p class="film__details">${movie.year}</p>
+                            <p class="film__details">Рейтинг IMDB: ${movie.ratingImdb}</p>
+                            <p class="film__details">Продолжительность: ${formatMovieLength(movie.filmLength)}</p>
+                            <p class="film__details">Страна: ${movie.countries[0]['country']}</p>
+                            <p class="film__text">${movie.description}</p>
+                    </div>
+                `
+
+                containerRight.insertAdjacentHTML('beforeend', html)   
+
+}
+
+function formatMovieLength(value) {
+    let length = '';
+
+    const hours = Math.floor(value / 60);
+    const minutes = value % 60;
+
+    if(hours > 0) length += hours + ' ч. '
+    if(minutes > 0) length += minutes + ' мин.'
+    console.log(length)
+
+    return length;
+   
+}
+formatMovieLength(127)
+   
 fetchAndRenderMovies().catch(err => console.log(err));
